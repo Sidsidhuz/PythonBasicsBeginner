3e02b653088111441671035093d87f82599a7b90
SIDHARTH <98815208+Sidsidhuz@users.noreply.github.com>
2025-06-20 04:35:43 +0530

    0.0

--- Comprehension.py
+++ Comprehension.py
@@ -1,41 +1,41 @@
-# comprehension
-import math,pyttsx3
-l = [i**2 for i in range(10)]
-print( [int(math.sqrt(i)) for i in l] )
-eve = [i for i in l if i %2 == 0]
-print(eve)
+# # comprehension
+# import math,pyttsx3
+# l = [i**2 for i in range(10)]
+# print( [int(math.sqrt(i)) for i in l] )
+# eve = [i for i in l if i %2 == 0]
+# print(eve)
+#
+# def speak(text,lang='en'):
+#     engine = pyttsx3.init()
+#     engine.say(text)
+#     engine.runAndWait()
+#
+# l = ['devin','dainty','david']
+# m = ['abin','ram','luke','jabbar']
+# k = [f"{i} {j}" for i in l for j in m]
+#
+# print(k)
+# # speak(k)
+# # for i in l:
+# #     for j in m:
+# #         print(i,'',j, end =' , ')
+#
+# # l = [1,2,3,4,5]
+# # m = [i**2 for i in l]
+# # d =  dict(zip(l,m))
+# # print(d)
 
-def speak(text,lang='en'):
-    engine = pyttsx3.init()
-    engine.say(text)
-    engine.runAndWait()
+key = ['devin','dainty','david']
+value = ['python','cyber','analyst']
+print({i:j for i,j in zip(key,value)})
+ls = [1,2,3,4,5,6]
+res = tuple(i for i in ls if i % 2 == 0)
+print(res)
 
-l = ['devin','dainty','david']
-m = ['abin','ram','luke','jabbar']
-k = [f"{i} {j}" for i in l for j in m]
+p = zip(key,value)
 
-print(k)
-# speak(k)
-# for i in l:
-#     for j in m:
-#         print(i,'',j, end =' , ')
+print(next(p))
+print(next(p))
 
-l = [1,2,3,4,5]
-m = [i**2 for i in l]
-d =  dict(zip(l,m))
-print(d)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(iter(key))
 
