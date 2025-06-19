0da86f3fc65ebe7c3c72f3b57b41daaea0f48a85
SIDHARTH <98815208+Sidsidhuz@users.noreply.github.com>
2025-06-20 05:07:02 +0530

    0.0

--- ExceptionHandling.py
+++ ExceptionHandling.py
@@ -1,11 +1,22 @@
-try:
-    a = int(input("Enter element a :"))
-    b = int(input("Enter element b :"))
-    res = a/b
-    print(res)
+t = 1
+while t:
 
-except:
-    print("b should be non 0 element")
-    pass
-finally:
-    print("HAI")
+    try:
+        print("Division")
+        a = int(input("Enter element a :"))
+        b = int(input("Enter element b :"))
+        res = a/b
+        print(res)
+        t = 0
+    except(ZeroDivisionError):
+        print("Check the input and try again")
+        pass
+# def div():
+#     a = int(input("Enter element a :"))
+#     b = int(input("Enter element b :"))
+#     res = a / b
+#     print(res)
+# try :
+#     div()
+# except :
+#     print("Division error!! Try Again")
