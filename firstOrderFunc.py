75e299ea9577802086be9c18dfca119938d6940e
SIDHARTH <98815208+Sidsidhuz@users.noreply.github.com>
2025-07-10 10:40:47 +0530

    0.0

--- firstOrderFunc.py
+++ firstOrderFunc.py
@@ -1,20 +1,40 @@
 # First Order function
 # handling function as a variable
 
+##############################################################################
+# def add(x,y):
+#     return x+y
+# var = add
+# print(var(3,4))
+
+###############################################################################################
+
 # def samp(name):
 #     return f"HELLO {name}"
 # def demo(func,name):
 #     return func(name)
 # print(demo(samp,'sid'))
 
-##########################################################################################
-def add(x,y):
-    return x+y
-def sub(x,y):
-    return x-y
-def mul(x,y):
-    return x*y
 
-ls = [add,sub,mul]
-for i in ls:
-    print(i(10,5))
+##########################################################################################
+# def add(x,y):
+#     return x+y
+# def sub(x,y):
+#     return x-y
+# def mul(x,y):
+#     return x*y
+#
+# ls = [add,sub,mul]
+# for i in ls:
+#     print(i(10,5))
+
+############################################################################################
+        # Higher Order Function #
+
+# def sqr(x):
+#     return x ** 2
+#
+# def mymap(func,args):
+#     for i in args:
+#         print(func(i))
+# mymap(sqr,[1,2,3])
