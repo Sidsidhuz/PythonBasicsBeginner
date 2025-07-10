1d59a758f3ec6f0121bf756a64c3a9b6e06b35a7
SIDHARTH <98815208+Sidsidhuz@users.noreply.github.com>
2025-07-10 10:34:51 +0530

    0.0

--- firstOrderFunc.py
+++ firstOrderFunc.py
@@ -1,5 +1,20 @@
-def samp(name):
-    return f"HELLO {name}"
-def demo(func,name):
-    return func(name)
-print(demo(samp,'sid'))
+# First Order function
+# handling function as a variable
+
+# def samp(name):
+#     return f"HELLO {name}"
+# def demo(func,name):
+#     return func(name)
+# print(demo(samp,'sid'))
+
+##########################################################################################
+def add(x,y):
+    return x+y
+def sub(x,y):
+    return x-y
+def mul(x,y):
+    return x*y
+
+ls = [add,sub,mul]
+for i in ls:
+    print(i(10,5))
